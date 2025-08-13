69dda30677534942a802a40cb2dc103d43ec077e
JamishaBade <badejamishagmail.com>
2025-08-13 09:46:43 -0400

    abstraction and encapsulation

---
+++ src/OOP/abstraction/example.java
@@ -0,0 +1,18 @@
+package OOP.abstraction;
+
+abstract class animal {
+
+  public void walk() {
+  }
+
+}
+
+class horse extends animal {
+  public void walk() {
+  }
+}
+
+class chicken extends animal {
+  public void walk() {
+  }
+}
