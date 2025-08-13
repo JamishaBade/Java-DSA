1e56636e03fdf87d214c5a4653adc96485fa229b
JamishaBade <badejamishagmail.com>
2025-08-13 01:18:01 -0400

    some modification for ArrayList

--- src/arrayList/reverseArrayList.java
+++ src/arrayList/reverseArrayList.java
@@ -1,6 +1,7 @@
 package arrayList;
 
 import java.util.ArrayList;
+import java.util.Collections; //inbuilt method to reverse
 
 /*input- [0,10,3,5,22,10]
 // output-[10,22,5,3,10,0]
@@ -23,13 +24,34 @@ public class reverseArrayList {
 
   public static void main(String[] args) {
     ArrayList<Integer> listValue = new ArrayList<>();
+    ArrayList<String> listNames = new ArrayList<>();
     listValue.add(0);
     listValue.add(10);
     listValue.add(3);
     listValue.add(5);
     listValue.add(22);
     listValue.add(10);
+
+    listNames.add("Jamisha");
+    listNames.add("James");
+    listNames.add("Maya");
+    listNames.add("Helen");
+    // function made
     reverseList(listValue);
+
+    // inbuilt reverse function used in java.util.Collections;
+    Collections.reverse(listNames);
+    System.out.println(listNames);
+
+    // sorting
+
+    // ascending order
+    Collections.sort(listValue);
+    System.out.println(listValue);
+    // descending order
+    Collections.sort(listValue, Collections.reverseOrder());
+    System.out.println(listValue);
+
   }
 
 }
