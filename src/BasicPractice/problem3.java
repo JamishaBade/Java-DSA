6bfd8a0a8e6cac11cffc835ed0441145155a5ecc
JamishaBade <badejamishagmail.com>
2025-08-12 23:39:52 -0400

    current exercises

--- src/BasicPractice/problem3.java
+++ src/BasicPractice/problem3.java
@@ -1,19 +1,22 @@
 package BasicPractice;
+
 import java.util.Scanner;
+
 public class problem3 {
-    public static int compareLarger(int a, int b){
-        if(a>b){
+    public static int compareLarger(int a, int b) {
+        if (a > b) {
             return a;
         }
         return b;
     }
+
     public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.println("Enter the two numbers");
         System.out.print("Enter the first number");
-        int a=input.nextInt();
+        int a = input.nextInt();
         System.out.print("Enter the first number");
-        int b=input.nextInt();
-        System.out.println("The larger number is "+ compareLarger(a,b));
+        int b = input.nextInt();
+        System.out.println("The larger number is " + compareLarger(a, b));
     }
 }
