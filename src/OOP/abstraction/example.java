8420eee8c85fa13c56e4979c880888e82b541b51
JamishaBade <badejamishagmail.com>
2025-08-13 09:52:49 -0400

    modification in abstraction

--- src/OOP/abstraction/example.java
+++ src/OOP/abstraction/example.java
@@ -1,18 +1,22 @@
 package OOP.abstraction;
 
 abstract class animal {
+  // abstraction= hiding unnecessary details adn showing only essential features
 
   public void walk() {
-  }
+  } // this is unnessary because we dont create instances of animal i general so we
+    // make it abstract.
 
 }
 
 class horse extends animal {
   public void walk() {
+    System.out.println("walks on 4 legs");
   }
 }
 
 class chicken extends animal {
   public void walk() {
+    System.out.println("walks on 2 legs");
   }
 }
