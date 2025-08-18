73f5c494916b1b73fb2ff9947ed1c7882c8a48e5
JamishaBade <badejamishagmail.com>
2025-08-17 22:04:34 -0400

    random md file testing

---
+++ src/Array/ProblemSolvingBruteForce/jkajfd.md
@@ -0,0 +1,29 @@
+# Welcome to My Test Page
+
+This is a **Markdown test page** to see how it looks on your website.
+
+---
+
+## Section 1: Introduction
+
+Markdown lets you write text in a **simple format** that can include:
+
+- Headers
+- Bold and _italic_ text
+- Links
+- Images
+- Code blocks
+- Lists
+
+---
+
+## Section 2: Code Example
+
+Here’s a small JavaScript example:
+
+```javascript
+function greet(name) {
+  console.log("Hello, " + name + "!");
+}
+greet("World");
+```
--- src/Array/prefixSumProblems/prefixSum.java
+++ src/Array/prefixSumProblems/prefixSum.java
@@ -4,17 +4,19 @@ import Imp.PublicMethods;
 
 import java.util.Arrays;
 
+//A prefix sum is an array where each element is the sum of all elements from the start up to that position.
 public class prefixSum {
     public static void main(String[] args) {
-       int n= PublicMethods.inputNumber();
-        int[] arr=PublicMethods.inputArrayValue(n);
-        int[] newArr=prefixSumArray(arr);
+        int n = PublicMethods.inputNumber();
+        int[] arr = PublicMethods.inputArrayValue(n);
+        int[] newArr = prefixSumArray(arr);
         System.out.println(Arrays.toString(newArr));
     }
-    static int[] prefixSumArray(int[] arr){
 
-        for(int i=1;i<arr.length;i++){
-            arr[i]=arr[i-1]+arr[i];
+    static int[] prefixSumArray(int[] arr) {
+
+        for (int i = 1; i < arr.length; i++) {
+            arr[i] = arr[i - 1] + arr[i];
         }
         return arr;
     }
